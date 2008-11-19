42a8a2f0e0f281b07986bc5e51550d5c5fbcc1ba
Will Benton <wibenton@redhat.com>
2008-11-19 11:04:32 -0600

    finds all commits in a specified repo

--- ocx.rb
+++ ocx.rb
@@ -1,15 +1,20 @@
 #!/usr/bin/env ruby
 # spetskhran -- catalogs and finds things that have been erased from history
 
+require 'rubygems'
 require 'grit'
 require 'digest/sha1'
 require 'set'
+
+include Grit
+
+DEBUG=false
 
 def ancestors(commit, seen)
   worklist = [commit]
   worklist.each do |cm|
     seen.merge([cm].to_set)
-    print $headnm, ": ", cm, " --> ", seen.length(), "\n"
+    print $headnm, ": ", cm, " --> ", seen.length(), "\n" if DEBUG
     cm.parents.each {|parent| worklist << parent if not seen.include?(parent) }
   end
   seen
@@ -18,8 +23,12 @@ end
 def all_commits(repo)
   seen = [].to_set
   repo.heads.each do |head| 
-    $headnm=head.name
+    $headnm=head.name if DEBUG
     seen.merge(ancestors(head.commit, seen)) 
   end
   seen
 end
+
+repo = Repo.new(ARGV[0])
+
+all_commits(repo).each {|commit| print commit, "\n"}
